7ca3f197db9be5088179a0ffb96f402c7c5ad6cc
lenrd <leonardo_aguiar@live.co.kr>
2020-08-03 23:07:19 -0300

    adiciona simple-slide plugin ao site

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if (window.SimpleSlide) {
+
+  new SimpleSlide({
+    slide: "quote", // nome do slide
+    nav: false, // se tem nagegação
+    auto: true, // se é automático
+    time: 5000 // tempo
+
+  })
+  new SimpleSlide({
+    slide: "portifolio",
+    nav: true,
+    auto: true,
+    time: 5000
+  })
+}
